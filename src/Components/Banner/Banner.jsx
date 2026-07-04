f6e7544669b00632075f6e075ffe9a6c230b34a0
Mainul-J <mainulh06@gmail.com>
2026-07-04 13:30:47 +0600

    testing and develop on free hosting

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,19 @@
-
+import './banner.css'
 
 const Banner = () => {
     return (
-        <div>
+        <div className='back '>
             <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage:
-      "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-  }}
+  className="relative"
 >
   <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+  <div className="hero-content text-neutral-content">
+    <div className="max-w-md -scale-x-100 absolute right-44 top-24">
+      <h1 className="mb-5 text-5xl font-bold   ">Bid on Unique Items from <br /> Around the world</h1>
       <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+        Discover rare collectibles, luxury goods, and vintage <br /> treasures in our curated auction
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn px-10 rounded-4xl text-xl">Explore Auction</button>
     </div>
   </div>
 </div>
